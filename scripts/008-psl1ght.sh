c098691d22667d58b2783e8c9311561461815109
StrawFox64 <122846274+StrawFox64@users.noreply.github.com>
2024-02-01 01:18:57 -0300

    download the psl1ght from my fork

--- scripts/008-psl1ght.sh
+++ scripts/008-psl1ght.sh
@@ -2,12 +2,15 @@
 # psl1ght.sh by Naomi Peori (naomi@peori.ca)
 
 ## Download the source code.
-wget --no-check-certificate https://github.com/ps3dev/PSL1GHT/tarball/master -O psl1ght.tar.gz
-
-## Unpack the source code.
-rm -Rf psl1ght && mkdir psl1ght && tar --strip-components=1 --directory=psl1ght -xvzf psl1ght.tar.gz
+wget --no-check-certificate https://github.com/StrawFox64/PSL1GHT/tarball/master -O psl1ght.tar.gz
 
 ## Create the build directory.
+rm -Rf psl1ght && mkdir psl1ght
+
+## Unpack the source code.
+tar --strip-components=1 --directory=psl1ght -xvzf psl1ght.tar.gz
+
+## Open directory
 cd psl1ght
 
 ## Compile and install.
